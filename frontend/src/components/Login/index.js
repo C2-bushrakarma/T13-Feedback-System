db7d49752e6b2979f1f349cbf96d54560eb1eb9a
Bushra <77388684+bushra1997@users.noreply.github.com>
2022-05-19 18:44:42 +0300

    login component

---
+++ frontend/src/components/Login/index.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import axios from "axios";
+import "./style.css";
+import { useNavigate, Link, useParams } from "react-router-dom";
+
+const Login = () => {
+  const { userType } = useParams();
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const login = () => {
+    axios
+      .post(`http://localhost:5000/login`, {
+        email,
+        password,
+        userType: userType,
+      })
+      .then((result) => {
+        const { data } = result;
+        localStorage.setItem("token", data.token);
+        localStorage.setItem("userId", data.userId);
+        if (userType === "user") {
+          navigate(`/myRequest/${data.userId}`);
+        }
+        if (userType === "admin") {
+          navigate("/dashboard");
+        } else {
+          return;
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="login_form">
+      <div className="login">
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <Form.Text className="text-muted"></Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </Form.Group>
+          <div className="buttons">
+            <div className="register">
+              <Link to={`/register/${userType}`}>
+                <Button variant="primary">Register</Button>
+              </Link>
+            </div>
+            <div className="login_but">
+              <Button variant="primary" onClick={login}>
+                Login
+              </Button>
+            </div>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
+};
+export default Login;
